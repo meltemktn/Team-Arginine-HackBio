6a1f801549aa2434173361bbb8be302b50a7b97a
Meltem <63255967+meltemktn@users.noreply.github.com>
2025-02-07 11:34:30 +0300

    Add files via upload

---
+++ stage-0/stage-0_task_team_arginine_final.R
@@ -0,0 +1,26 @@
+# HackBio Internship Program - February, 2025
+# Team-arginine on the stage-0 task
+# The group consists of four people, namely:
+# Meltem (GitHub: @meltemktn)
+# BenAkande (GitHub: @BenAkande123)
+# Manav Vaish (GitHub: @manavvaish)
+# Favour_Imoniye (GitHub: @Favour-Imoniye)
+# GitHub link to the team-arginine repository for stage-0:
+# https://github.com/meltemktn/Team-Arginine-HackBio/tree/main/stage-0
+
+# Video tutorial URL:
+# https://shorturl.at/1Uxyo 
+
+# Create data frame 
+team_arginine_R <- data.frame(
+  Name = c("Benjamin Akande", "Meltem Kutnu", "Manav Vaish", "Favour Imoniye"),
+  Slack_username = c("benakande", "meltemktn18", "manavvaish", "Favour_Imoniye"),
+  Email = c("akandebenade@gmail.com", "meltemktn18@gmail.com", "manavvaish@myyahoo.com", "favourimoniye484@gmail.com"),
+  Hobby = c("Cooking", "Photography, solving crossword puzzles, watching movies", "Coding", "Reading fiction"),
+  Country = c("Ghana", "Turkey", "India", "Nigeria"),
+  Discipline = c("Developmental biology", "Molecular biology and genetics (microbiology)", "Biotechnology", "Microbiology"),
+  Programming_language = c("R", "R", "R", "R")
+)
+
+# Print team_arginine_R
+print(team_arginine_R)
